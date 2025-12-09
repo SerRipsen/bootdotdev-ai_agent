0f45a398cffba1b58007a6b151be25d06942b3ad
Ser Ripsen <ser@ripsen.com>
2025-12-09 01:06:42 +0000

    Added: Main API query and connection to Gemini, responds to a single prompt given at runtime. Also displays Prompt Tokens and Response Tokens used.

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+import argparse
+from dotenv import load_dotenv
+from google import genai
+
+
+def main():
+    print("Initialising AI Agent")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key is None:
+        raise RuntimeError("No API key found")
+    client = genai.Client(api_key=api_key)
+
+    # Allow for user input
+    parser = argparse.ArgumentParser(description="AI Agent")
+    parser.add_argument("prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
+    print("Asking initial question")
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=args.prompt)
+    if response.usage_metadata is None: raise RuntimeError("Failed API request")
+    print(f"""
+User prompt: {args.prompt}
+Prompt tokens: {response.usage_metadata.prompt_token_count}
+Response tokens: {response.usage_metadata.candidates_token_count}
+Response:
+{response.text}
+""")
+
+
+
+if __name__ == "__main__":
+    main()
