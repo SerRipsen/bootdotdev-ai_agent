5c3d28fd4630e11117c8a10e67f4392864ae41ce
Ser Ripsen <ser@ripsen.com>
2025-12-09 01:31:24 +0000

    Added: Verbose ouput as an argument

--- main.py
+++ main.py
@@ -15,22 +15,22 @@ def main():
 
     # Allow for user input
     parser = argparse.ArgumentParser(description="AI Agent")
-    parser.add_argument("prompt", type=str, help="User prompt")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
     # Handle a conversation
-    messages = [type.Content(role='user', parts=[types.Part(text=args.user+prompt)])]
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     print("Asking initial question")
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=messages)
     if response.usage_metadata is None: raise RuntimeError("Failed API request")
-    print(f"""
-User prompt: {args.prompt}
+    if args.verbose: print(f"""User prompt: {args.user_prompt}
 Prompt tokens: {response.usage_metadata.prompt_token_count}
-Response tokens: {response.usage_metadata.candidates_token_count}
-Response:
+Response tokens: {response.usage_metadata.candidates_token_count}""")
+    print(f"""Response:
 {response.text}
 """)
 
